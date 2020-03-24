d4f7671102d962b0c48aeab71db999c7ae87b5d6
Medhansh7 <medhanshchaubey7@gmail.com>
2020-03-24 15:40:45 +0530

    Added component, container and hoc. Code is still to be writeen

---
+++ src/containers/BurgerBuilder/Burger.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux';
+
+class Burger extends Component {
+    render() {
+        return (
+            <Aux>
+                <div>Burger</div>
+                <div>Builder</div>
+            </Aux>
+        );
+    }
+}
+
+export default Burger;
