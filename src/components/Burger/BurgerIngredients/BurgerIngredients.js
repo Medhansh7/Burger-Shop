e36d524891ca2d0168ecda2f9ab45b57f9c2e1fd
Medhansh7 <medhanshchaubey7@gmail.com>
2020-03-27 01:53:29 +0530

    added ingredients

---
+++ src/components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "./BurgerIngredients.css"
+
+class BurgerIngredients extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default BurgerIngredients;
